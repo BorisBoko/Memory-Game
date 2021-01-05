3c4fe82fedc1f7ad2374b1c593d442949141a689
BorisBoko <borispetrosyans8@gmail.com>
2021-01-05 17:00:18 +0400

    animate

--- main.js
+++ main.js
@@ -1,41 +1,30 @@
 let Allpng = [
-    "atom.png",
-    "calc.png",
-    "chrome.png",
-    "fox.png",
-    "tools_green.png",
-    "wifi.png",
-    "vlc.png",
-    "tools_orange.png",
-    "atom.png",
-    "calc.png",
-    "chrome.png",
-    "fox.png",
-    "tools_green.png",
-    "wifi.png",
-    "vlc.png",
-    "tools_orange.png",
+    "img/atom.png",
+    "img/calc.png",
+    "img/chrome.png",
+    "img/fox.png",
+    "img/tools_green.png",
+    "img/wifi.png",
+    "img/vlc.png",
+    "img/tools_orange.png",
+    "img/atom.png",
+    "img/calc.png",
+    "img/chrome.png",
+    "img/fox.png",
+    "img/tools_green.png",
+    "img/wifi.png",
+    "img/vlc.png",
+    "img/tools_orange.png",
 ]
 
 const startBtn = document.getElementById('start')
 const contenarTable = document.getElementById('containier')
+let shuffledd = Allpng
+    .map((a) => ({ sort: Math.random(), value: a }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
 
-// let shuffledd = Allpng
-//     .map((a) => ({ sort: Math.random(), value: a }))
-//     .sort((a, b) => a.sort - b.sort)
-//     .map((a) => a.value)
-
-// renderGame(shuffledd)
-
-shuffleCards = (a) => {
-    for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-  }
-  let shuffledd = shuffleCards(Allpng)
-  renderGame(shuffledd)
+renderGame(shuffledd)
 const clickHandler = function() {
     window.location.reload();
 }
@@ -43,6 +32,8 @@ const clickHandler = function() {
 startBtn.addEventListener('click', clickHandler);
 
 function renderGame(shuffled) {
+
+    //contenarTable.innerHTML = ''
     const template = renderTemplate(shuffled)
     contenarTable.insertAdjacentHTML('afterbegin', template);
 }
@@ -51,28 +42,28 @@ function renderTemplate(shuffled) {
     return `
     <table class="faded" border="0" cellspacing="1" cellpadding="1">
             <tr>
-                <td><img class='in_game ' data-picture="${shuffled[0]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game ' data-picture="${shuffled[1]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game ' data-picture="${shuffled[2]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game ' data-picture="${shuffled[3]}" data-noPng="noPng.png" src="noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[0]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[1]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[2]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[3]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
             </tr>
             <tr>
-                <td><img class='in_game' data-picture="${shuffled[4]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[5]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[6]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[7]}" data-noPng="noPng.png" src="noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[4]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[5]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[6]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[7]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
             </tr>
             <tr>
-                <td><img class='in_game' data-picture="${shuffled[8]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[9]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[10]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[11]}" data-noPng="noPng.png" src="noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[8]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[9]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[10]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[11]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
             </tr>
             <tr>
-                <td><img class='in_game' data-picture="${shuffled[12]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[13]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[14]}" data-noPng="noPng.png" src="noPng.png"></td>
-                <td><img class='in_game' data-picture="${shuffled[15]}" data-noPng="noPng.png" src="noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[12]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[13]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[14]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
+                <td><img class='in_game' data-picture="${shuffled[15]}" data-noPng="img/noPng.png" src="img/noPng.png"></td>
             </tr>
         </table>
     `
@@ -82,11 +73,11 @@ function renderTemplate(shuffled) {
 
 const table = document.querySelector('table')
 
-//Event click svg
 
 let clickToTable = function(event) {
+    const e = event.target
     if (event.target.localName === 'img') {
-        imgChange(event.target)
+        imgChange(e)
     }
 }
 
@@ -95,27 +86,23 @@ table.addEventListener('click', clickToTable)
 
 
 function imgChange(e) {
-    e.classList.add('animated', 'tada')
     const imgies = table.querySelectorAll('.in_game')
-    
-    const check = checkOneorTwo(e, imgies);
-    //change the src to dataset.picture
+    let check = checkOneorTwo(e, imgies);
     e.attributes.src.nodeValue = e.dataset.picture
-   
+    e.classList.add('animated', 'tada')
     if (check) {
-        //its for 3 item is not work
         changeClick()
 
         setTimeout(function() {
+
             e.attributes.src.nodeValue = e.dataset.nopng
-            e.classList.remove('animated', 'tada')
             hideAll(imgies)
-            //its for addeventListener work agian
             table.addEventListener('click', clickToTable)
+
         }, 700);
 
     }
-       
+
 
 }
 
@@ -124,42 +111,55 @@ function changeClick() {
 }
 
 function hideAll(imgies) {
-    
     imgies.forEach(element => {
-        element.classList.remove('animated', 'tada')
+
         element.attributes.src.nodeValue = element.dataset.nopng
+        element.classList.remove('animated', 'tada')
+
     });
 }
 
 function checkOneorTwo(e, imgies) {
-    console.log(imgies)
-    if(imgies.length === 2) {
-        alert('You Win')
+
+    let itemPicSet = e.dataset.picture
+        //console.log(itemPicSet)
+    if (e.attributes.src.nodeValue === itemPicSet) {
+        console.log(e.attributes.src.nodeValue, itemPicSet)
+
+        return false
     }
-    const inItemPicSet = e.dataset.picture
-    const inItemPicSrc = e.attributes.src.nodeValue
-    //it is for 2 click in 1 pic reture false
-    if (inItemPicSrc === inItemPicSet) return 
-    
-    const srcItem = [];
-    const rightcombination = [];
+
+    const srcitem = [];
+    const fixItem = [];
     let dontHide = false
+
+
+
     imgies.forEach(element => {
-        if (inItemPicSet === element.dataset.picture) {
-            rightcombination.push(element)
+        if (itemPicSet === element.dataset.picture) {
+            fixItem.push(element)
         }
-        srcItem.push(element.attributes.src.nodeValue)
+        if (element.attributes.src.nodeValue)
+            srcitem.push(element.attributes.src.nodeValue)
     });
-    console.log(rightcombination)
-    rightcombination.forEach(element => {
-        // if 2 right Combination is not noPng 
-        if (rightcombination[0].attributes.src.nodeValue !== rightcombination[1].attributes.src.nodeValue) {
+
+    fixItem.forEach(element => {
+        if (fixItem[0].attributes.src.nodeValue !== fixItem[1].attributes.src.nodeValue) {
             element.classList.remove('in_game')
+            element.classList.add('animated', 'rotateOut')
+
+
             dontHide = true
         }
     });
- 
-    let checkSrc = Array.from(new Set(srcItem))
 
-    if (checkSrc.length === 1 || dontHide) {return false} else return true 
+
+
+
+    let checkSrc = Array.from(new Set(srcitem))
+
+
+    if (checkSrc.length === 1 || dontHide) {
+        return false
+    } else { return true }
 }
